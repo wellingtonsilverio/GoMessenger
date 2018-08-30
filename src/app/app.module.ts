1023467791ca8a2fb66da203ba9ffcd0fdbfb578
Wellington Silverio <wellington.alves.silverio@gmail.com>
2018-08-30 16:23:24 -0300

    init and add css, firebase and function to send message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,22 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+var configFirebase = {
+  apiKey: "AIzaSyD4nP6veuwaNNTqihkxp9UUJDOMD6ITdtc",
+  authDomain: "gomessengerbr.firebaseapp.com",
+  databaseURL: "https://gomessengerbr.firebaseio.com",
+  projectId: "gomessengerbr",
+  storageBucket: "gomessengerbr.appspot.com",
+  messagingSenderId: "341954695291"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +27,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    FormsModule,
+    AngularFireModule.initializeApp(configFirebase),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  mode: any;
 
-  constructor(public navCtrl: NavController) {
+  user: string;
 
+  input = {message: ""};
+
+  messages: Observable<any[]>;
+
+  constructor(
+    public navCtrl: NavController,
+    public db: AngularFireDatabase
+  ) {
+    this.mode = 'global';
+
+    this.user = 'Wellington';
+
+    this.messages = this.db.list('/chats').valueChanges();
+  }
+
+  sendMessage(){
+    this.db.list('/chats').push({
+      user: this.user,
+      text: this.input.message
+    }).then( () => {
+      //success
+    });
+    this.input.message = "";
   }
 
 }
